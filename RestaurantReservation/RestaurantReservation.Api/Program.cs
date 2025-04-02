84467c8c0da4cc9c03e6c20d8361ee0e7d456e07
Nelson Ortega <nortega@restaurant365.com>
2025-04-02 11:05:10 -0500

    Service collection extensions

--- RestaurantReservation/RestaurantReservation.Api/Program.cs
+++ RestaurantReservation/RestaurantReservation.Api/Program.cs
@@ -2,23 +2,10 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using RestaurantReservation.Api;
 using RestaurantReservation.Api.Jwt;
-using RestaurantReservation.Domain.Repositories;
-using RestaurantReservation.Domain.Customers.Services;
-using RestaurantReservation.Domain.Employees.Services;
-using RestaurantReservation.Domain.MenuItems.Service;
-using RestaurantReservation.Domain.Orders.Services;
-using RestaurantReservation.Domain.Reservations.Services;
-using RestaurantReservation.Domain.Restaurants.Services;
-using RestaurantReservation.Domain.Tables.Services;
 using RestaurantReservation.Infrastructure.Contexts;
-using RestaurantReservation.Infrastructure.Customers.Repositories;
-using RestaurantReservation.Infrastructure.Employees.Repositories;
-using RestaurantReservation.Infrastructure.MenuItems.Repositories;
-using RestaurantReservation.Infrastructure.Orders.Repositories;
-using RestaurantReservation.Infrastructure.Reservations.Repositories;
-using RestaurantReservation.Infrastructure.Restaurants.Repositories;
-using RestaurantReservation.Infrastructure.Tables.Repositories;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,26 +23,9 @@ builder.Services.AddLogging();
 builder.Services.AddEndpointsApiExplorer();
 
 // Register repositories and services
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<ICustomerService, CustomerService>();
 
-builder.Services.AddScoped<IRestaurantRepository, RestaurantRepository>();
-builder.Services.AddScoped<IRestaurantService, RestaurantService>();
-
-builder.Services.AddScoped<ITableRepository, TableRepository>();
-builder.Services.AddScoped<ITableService, TableService>();
-
-builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-
-builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
-builder.Services.AddScoped<IReservationService, ReservationService>();
-
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-
-builder.Services.AddScoped<IMenuItemRepository, MenuItemRepository>();
-builder.Services.AddScoped<IMenuItemService, MenuItemService>();
+builder.Services.RegisterRepositories();
+builder.Services.RegisterServices();
 
 builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
 
---
+++ RestaurantReservation/RestaurantReservation.Api/ServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using RestaurantReservation.Domain.Customers.Services;
+using RestaurantReservation.Domain.Employees.Services;
+using RestaurantReservation.Domain.MenuItems.Service;
+using RestaurantReservation.Domain.Orders.Services;
+using RestaurantReservation.Domain.Repositories;
+using RestaurantReservation.Domain.Reservations.Services;
+using RestaurantReservation.Domain.Restaurants.Services;
+using RestaurantReservation.Domain.Tables.Services;
+using RestaurantReservation.Infrastructure.Customers.Repositories;
+using RestaurantReservation.Infrastructure.Employees.Repositories;
+using RestaurantReservation.Infrastructure.MenuItems.Repositories;
+using RestaurantReservation.Infrastructure.Orders.Repositories;
+using RestaurantReservation.Infrastructure.Reservations.Repositories;
+using RestaurantReservation.Infrastructure.Restaurants.Repositories;
+using RestaurantReservation.Infrastructure.Tables.Repositories;
+
+namespace RestaurantReservation.Api;
+
+public static class ServiceCollectionExtensions
+{
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICustomerService, CustomerService>();
+        services.AddScoped<IRestaurantService, RestaurantService>();
+        services.AddScoped<ITableService, TableService>();
+        services.AddScoped<IEmployeeService, EmployeeService>();
+        services.AddScoped<IReservationService, ReservationService>();
+        services.AddScoped<IOrderService, OrderService>();
+        services.AddScoped<IMenuItemService, MenuItemService>();
+    }
+
+    public static void RegisterRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IRestaurantRepository, RestaurantRepository>();
+        services.AddScoped<ITableRepository, TableRepository>();
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IReservationRepository, ReservationRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IMenuItemRepository, MenuItemRepository>();
+    }
+}

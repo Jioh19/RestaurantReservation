68e9e15225d780d44aeeb044ece053a834482868
jioh19 <jioh1982@gmail.com>
2025-04-04 00:10:36 -0300

    feat: Add Customer model validations
    - Add presence validation for name, email, and phone
    - Add email format validation

--- RestaurantReservation/RestaurantReservation.Api/Program.cs
+++ RestaurantReservation/RestaurantReservation.Api/Program.cs
@@ -1,9 +1,13 @@
 using System.Text;
+using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using RestaurantReservation.Api;
+using RestaurantReservation.Api.Contracts.Customers.Models;
+using RestaurantReservation.Api.Customers.Validators;
 using RestaurantReservation.Api.Jwt;
+using RestaurantReservation.Domain.Customers.Models;
 using RestaurantReservation.Infrastructure.Contexts;
 
 
@@ -16,8 +20,8 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<RestaurantReservationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
         x => x.MigrationsAssembly(typeof(RestaurantReservationDbContext).Assembly)
-        )
-    );
+    )
+);
 builder.Services.AddControllers();
 builder.Services.AddLogging();
 builder.Services.AddEndpointsApiExplorer();
@@ -31,6 +35,8 @@ builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
 
 builder.Services.Configure<JwtSettings>(
     builder.Configuration.GetSection(nameof(JwtSettings)));
+
+builder.Services.AddScoped<IValidator<CustomerRequest>, CustomerValidator>();
 
 builder.Services.AddAuthentication(options =>
     {
@@ -68,10 +74,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 #pragma warning disable ASP0014
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 #pragma warning restore ASP0014
 
 app.Run();
